cfa450cef2e0cc3fffdca7075bcecec42bc3f6b8
HelenaHalldinITHS <helena.halldin@iths.se>
2022-04-25 10:01:44 +0200

    Add POJO Conference

---
+++ src/main/java/com/example/Conference.java
@@ -0,0 +1,16 @@
+package com.example;
+import io.micronaut.core.annotation.Introspected;
+
+@Introspected
+public class Conference {
+
+    private final String name;
+
+    public Conference(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
